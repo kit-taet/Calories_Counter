22e872513cb6ee6414597f41a51089b802fdd740
kit-taet <tkid226@gmail.com>
2020-04-28 21:20:18 +1000

    add new file

---
+++ terminal_app.rb
@@ -0,0 +1,84 @@
+require 'colorize'
+require 'tty-prompt'
+require 'artii'
+require 'rainbow'
+require 'colorized_string'
+require 'faker'
+
+
+require_relative "./classes/calculate.rb"
+require_relative "./classes/foodlist.rb"
+
+
+
+
+
+#greet user
+def greet
+  hm = Artii::Base.new :font => 'slant'
+  puts (hm.asciify("Calories Counter")).colorize(:magenta)
+end
+
+
+def menu
+
+  puts ("")
+  puts "Let's count some calories!!".colorize(:brown)
+  puts ("-----------------------------------------------------------------------").colorize(:yellow)
+  puts ("Please select these options 1, 2 or 3")
+  puts ("-----------------------------------------------------------------------").colorize(:red)
+  puts ("")
+
+  puts ("1.") + " Calculate"
+  puts ("2.") + " Food List"
+  puts ("3.") + " Quit"
+
+end
+
+
+#get user options
+def useroptions
+
+userop = gets.chomp
+
+while userop
+
+    case userop
+
+            when "1"
+              calculate_cal
+              clear
+
+            when "2"
+              list
+              clear
+
+            when "3"
+              sleep 0.5
+              system 'clear'
+              doneplaying = Artii::Base.new :font => 'slant'
+              puts (doneplaying.asciify("Thankyou For Using")).colorize(:cyan)
+              return 0
+              break
+
+            else  
+              puts "Please choose your options again 1,2 or 3".colorize(:red)
+              puts ("-----------------------------------------------------------------------")
+              sleep 0.5
+              system 'clear'
+              return greet, menu, useroptions
+              
+            end
+  end
+end
+
+def clear
+  system "clear"
+end
+
+greet
+menu
+useroptions
+clear
+
+
